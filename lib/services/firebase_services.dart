55e45b52071c82f3fd2fe84a93a546b927f3d726
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2023-09-28 12:41:30 +0600

    Google Sign in method with firebase implemented

--- lib/services/firebase_services.dart
+++ lib/services/firebase_services.dart
@@ -4,6 +4,43 @@ import 'package:google_sign_in/google_sign_in.dart';
 class FirebaseServices{
   final _auth = FirebaseAuth.instance;
   final _googleSignIn = GoogleSignIn();
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+// Create a new credential
+
+// Once signed in, return the UserCredential
+      return await _auth.signInWithCredential(credential);
+
+
+  }
+
+
+
+  signout() async{
+    await _auth.signOut();
+    await _googleSignIn.signOut();
+  }
+}
+
+/*
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class FirebaseServices{
+  final _auth = FirebaseAuth.instance;
+  final _googleSignIn = GoogleSignIn();
 
   signInWithGoogle() async {
     try {
@@ -19,7 +56,7 @@ class FirebaseServices{
       }
     } on FirebaseAuthException catch (e) {
 
-      print(e.message);
+      print("Error message -> ${e.message}");
       throw e;
     }
   }
@@ -28,4 +65,4 @@ class FirebaseServices{
     await _auth.signOut();
     await _googleSignIn.signOut();
   }
-}
+}*/
